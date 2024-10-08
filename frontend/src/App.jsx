65ac7b0c3db85ba5b2b2f3564e78af0878770fbc
shruti0101 <shrutiguptacu@gmail.com>
2024-10-08 17:57:22 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./components/about/About"
 import Layout from "./Layout";
 import Home from "./components/home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
-import './App.css'
+
 
 function App() {
  
 
   return (
     <>
+
+    <Navbar></Navbar>
     <BrowserRouter>
+    
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<div>page not found</div>}></Route>
      
       </Route>
     </Routes>
